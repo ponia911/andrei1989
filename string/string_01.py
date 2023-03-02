ba6c3965da408dde9a30960a33ee2a3d9c31aac8
Andrei <poniaable@gmail.com>
2023-03-02 12:41:12 +0300

    string_01

---
+++ string/string_01.py
@@ -0,0 +1,13 @@
+a = "Hello"
+b = "my"
+c = "name"
+d = "is"
+e = "Andrei"
+f = "Posakh"
+g = "i'm"
+h = "33"
+i = "years"
+j = "old"
+print(a + "," + b + " " + c + " " + d + " " + e + " " + f + "," + g + " " + h + " " + i + " " +j)
+k = (a + "," + b + " " + c + " " + d + " " + e + " " + f + "," + g + " " + h + " " + i + " " +j)
+print(len(k))
