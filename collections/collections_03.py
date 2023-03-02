f60dfabf49494df020f8f2144d9d65389f1a8c71
Andrei <poniaable@gmail.com>
2023-03-02 20:47:21 +0300

    collections_03

---
+++ collections/collections_03.py
@@ -0,0 +1,9 @@
+my_lisst = [2, 4, 16, "Andrei"]
+print(my_lisst)
+my_lisst2 = [5, 4, "Posakh", 99]
+print(my_lisst2)
+my_lisst.extend(my_lisst2)
+print(my_lisst)
+my_lisst.insert(4, "123")
+print(my_lisst)
+print(len(my_lisst))
