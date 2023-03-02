69c16b42ab412f328bc1806ae700dd0f818915de
Andrei <poniaable@gmail.com>
2023-03-02 20:03:49 +0300

    collections_02

---
+++ collections/collections_02.py
@@ -0,0 +1,10 @@
+my_lisst = [1, 4, 16, 23, "pop"]
+my_lisst.append("Hello")
+print(my_lisst)
+my_lisst.pop(0)
+print(my_lisst)
+my_lisst[0] = "World"
+print(my_lisst)
+my_lisst.remove("World")
+print(my_lisst)
+print(my_lisst[::-1])
