b2db89d7df7b0197fdabae1757500d1ff246cd33
Andrei <poniaable@gmail.com>
2023-03-02 19:02:21 +0300

    string_02

--- string/string_02.py
+++ string/string_02.py
@@ -2,3 +2,4 @@ my_string = "Manchester"
 x = "Manchester"
 print(x)
 print((x[:6]))
+print((x[0:6:1]))
--- string/string_04.py
+++ string/string_04.py
@@ -1,4 +1,4 @@
 my_string = "0123456789"
 x = "0123456789"
-print(my_string[1:len(my_string):2])
 print(x[1:len(x):2])
+
