7ee953b6f6695481c8e155dd8aaa3ee1c9729862
Andrei <poniaable@gmail.com>
2023-03-02 17:28:11 +0300

    string_01

--- string/string_01.py
+++ string/string_01.py
@@ -1,13 +1,7 @@
-a = "Hello"
-b = "my"
-c = "name"
-d = "is"
-e = "Andrei"
-f = "Posakh"
-g = "i'm"
-h = "33"
-i = "years"
-j = "old"
-print(a + "," + b + " " + c + " " + d + " " + e + " " + f + "," + g + " " + h + " " + i + " " +j)
-k = (a + "," + b + " " + c + " " + d + " " + e + " " + f + "," + g + " " + h + " " + i + " " +j)
-print(len(k))
+a = "Hi"
+b = "i'm"
+c = "Andrei"
+d = "Posakh"
+x = (a + "," + b + " " + c + " " + d)
+print(x)
+print(len(x))
