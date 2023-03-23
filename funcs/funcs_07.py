6684236704a5899353dd1a667e28892ec114aa77
Andrei <poniaable@gmail.com>
2023-03-23 20:56:33 +0300

    funcs_07

---
+++ funcs/funcs_07.py
@@ -0,0 +1,16 @@
+#Создать функцию,
+# которая принимает на вход неопределенное количество именованных аргументов
+# и выводит на экран те из них, длина ключа которых четна.
+
+
+def print_from_dict(**kargs):
+    print(kwargs, type(kwargs))
+    for key, value in kwargs.items():
+        if len(key) % 2 == 0:
+            print(key, value)
+
+def main():
+    print(print_from_dict(a=1, bb=2, ccc=3, dddd=4, sssss=5, aaaaaa=6)
+
+if __name__ == '__main__':
+   main()
