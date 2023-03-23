530edd423f1792a35f4b429904c3d305a153106e
Andrei <poniaable@gmail.com>
2023-03-23 19:42:48 +0300

    funcs_02

--- funcs/funcs_02.py
+++ funcs/funcs_02.py
@@ -5,3 +5,27 @@
 #Нахождение максимального элемента
 #Нахождение минимального элемента.
 
+from random import randint
+
+#matrix = [[randint(1, 10) for _  in range(n) for _ in range(n)]]
+def create_matrix(n):
+    arr = []
+        for i in range(n):
+                arr_in = []
+                for i in range(n):
+
+                    arr_in.append(randint(0, 9))
+
+                    matrix.append(arr_in)
+                return matrix
+        print(arr)
+
+    for arr in matrix:
+        result += sum(arr)
+        result = sum([sum(arr) for arr in matrix])
+        return result
+
+def max_element(matrix):
+    list_of_max_element = []
+     for arr in matrix:
+        max_element_in
