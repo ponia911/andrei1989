1306df37302c8d7beef89fb2e57e5ba8c638d718
Andrei <poniaable@gmail.com>
2023-03-27 21:14:45 +0300

    funcs_prog_07

--- funcs_prog/funcs_prog_07.py
+++ funcs_prog/funcs_prog_07.py
@@ -1,7 +1,22 @@
 #Дан список чисел.
 # Найти произведение всех чисел, которые кратны 3.
-import reduce
-arr = []
+from functools import reduce
 
-result = reduce(lambda x, y: x + y, arr, 1)
+arr = [i for  i in range(10)]
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+new_arr = [i for i in arr if i % 3 == 0]
+print(new_arr)
+
+# version 2
+new_arr = filter(lambda x, y: x * y, filter(lambda x: x % 3 == 0, arr))
+result = reduce(lambda x, y: x * y, new_arr)
+print(result)
+
+result = reduce(lambda x, y: x * y, filter(lambda x: x % 3 == 0, new_arr))
+print(result)
+
+#version 3
+
+
+result = reduce(lambda x, y: x * y, filter(lambda x: x% 3 == 0, arr))
 print(result)
