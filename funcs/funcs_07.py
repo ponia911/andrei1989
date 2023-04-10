91e8832dacf4155d2d3b8587d9db6462d7015fd9
Andrei <poniaable@gmail.com>
2023-04-10 22:10:45 +0300

    funcs change

--- funcs/funcs_07.py
+++ funcs/funcs_07.py
@@ -1,16 +1,17 @@
-#Создать функцию,
+# Создать функцию,
 # которая принимает на вход неопределенное количество именованных аргументов
 # и выводит на экран те из них, длина ключа которых четна.
 
 
-def print_from_dict(**kargs):
+def print_from_dict(**kwargs):
     print(kwargs, type(kwargs))
     for key, value in kwargs.items():
         if len(key) % 2 == 0:
             print(key, value)
 
-def main():
-    print(print_from_dict(a=1, bb=2, ccc=3, dddd=4, sssss=5, aaaaaa=6)
 
-if __name__ == '__main__':
-   main()
+def main():
+    print(print_from_dict(a=1, bb=2, ccc=3, dddd=4, sssss=5, aaaaaa=6))
+
+    if __name__ == '__main__':
+        main()
